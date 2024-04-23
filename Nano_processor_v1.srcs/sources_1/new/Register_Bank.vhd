97a5d526f7f408e86691166fce42f033aa13480f
vinukab <162571762+vinukab@users.noreply.github.com>
2024-04-23 23:31:36 +0530

    error detected in register bank output

--- Nano_processor_v1.srcs/sources_1/new/Register_Bank.vhd
+++ Nano_processor_v1.srcs/sources_1/new/Register_Bank.vhd
@@ -43,6 +43,7 @@ entity Register_Bank is
            R_5_val : out STD_LOGIC_VECTOR (3 downto 0);
            R_6_val : out STD_LOGIC_VECTOR (3 downto 0);
            R_7_val : out STD_LOGIC_VECTOR (3 downto 0)
+           
            );  -- Output data from selected register
 end Register_Bank;
 
