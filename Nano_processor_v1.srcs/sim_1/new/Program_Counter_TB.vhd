196f5843302bc652035653403376becfd636fdca
Seniru Dilmith <dilmithseniru@gmail.com>
2024-05-05 16:40:58 +0530

    v1 finalized
    made some amendments. now finalized

--- Nano_processor_v1.srcs/sim_1/new/Program_Counter_TB.vhd
+++ Nano_processor_v1.srcs/sim_1/new/Program_Counter_TB.vhd
@@ -1,5 +1,33 @@
+-- Company:
+-- Engineer:
+-- 
+-- Create Date: 04/18/2024 11:06:02 PM
+-- Design Name:
+-- Module Name: Program_counter_TB - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity Program_counter_TB is
 end Program_counter_TB;
